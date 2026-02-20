34a83f4158cb06e0b1ab5287d28efc046bcd464b
ankofl <ankofl@yandex.ru>
2026-02-20 19:09:06 +0300

    Рефакторинг Wheel

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -1,15 +1,33 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Transform))]
 public class Wheel : MonoBehaviour
 {
-	[SerializeField]
+	/// <summary>
+	/// Радиус колеса
+	/// </summary>
+	[SerializeField, Tooltip("Радиус колеса")]
 	private float radius = 1;
 
+	/// <summary>
+	/// Текущий угол вращения колеса
+	/// </summary>
+	private float wheelRotation = 0;
+
+	/// <summary>
+	/// Устанавливает вращение колеса
+	/// </summary>
+	/// <param name="speed">Скорость движения погрузчика</param>
+	/// <param name="steerWheelAngle">Угол поворота руля в градусах</param>
 	public void SetRotation(float speed, float steerWheelAngle)
 	{
-		WheelRotation += speed / (radius * 2) * Mathf.Rad2Deg * Time.fixedDeltaTime;
+		// Вычисляем смещение вращения колеса за кадр
+		float deltaRotation = speed / (radius * 2) * Mathf.Rad2Deg * Time.fixedDeltaTime;
 
-		transform.localRotation = Quaternion.Euler(WheelRotation, steerWheelAngle, 0f);
+		// Обновляем текущий угол вращения
+		wheelRotation += deltaRotation;
+
+		// Применяем вращение и угол поворота к трансформу колеса
+		transform.localRotation = Quaternion.Euler(wheelRotation, steerWheelAngle, 0f);
 	}
-	private float WheelRotation = 0;
 }
